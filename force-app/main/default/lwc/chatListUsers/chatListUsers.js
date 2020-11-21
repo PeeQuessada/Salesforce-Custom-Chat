94322959793ad39bad40a9911caf0132f891fbbe
PeeQuessada <pedro.rafael.quessada@gmail.com>
2020-11-21 02:56:49 -0300

    Definicao e personalizacao do scrool

--- force-app/main/default/lwc/chatListUsers/chatListUsers.js
+++ force-app/main/default/lwc/chatListUsers/chatListUsers.js
@@ -4,12 +4,15 @@ export default class ChatListUsers extends LightningElement {
   get fakeList() {
     return [
       { name: "Marcio", inicial: "MA" },
-      { name: "Joaquim", inicial: "JO" },
+      { name: "Jander", inicial: "JA" },
       { name: "Alice", inicial: "AL" },
       { name: "Renato", inicial: "RE" },
       { name: "Beatriz", inicial: "BE" },
       { name: "Marcos", inicial: "MA" },
-      { name: "Afonso", inicial: "AF" }
+      { name: "Afonso", inicial: "AF" },
+      { name: "Pedro", inicial: "PE" },
+      { name: "Mulan", inicial: "MU" },
+      { name: "Gisele", inicial: "GI" }
     ];
   }
 }
